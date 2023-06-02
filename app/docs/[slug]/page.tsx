7e768c11f93f493554840a59ceccdcc2598d0a23
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-06-03 00:13:39 +0900

    update

--- app/docs/[slug]/page.tsx
+++ app/docs/[slug]/page.tsx
@@ -18,7 +18,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <div className="prose prose-h2:mt-16 prose-img:rounded-md prose-sm prose-code:text-sm prose-pre:bg-transparent max-w-full dark:prose-invert prose-pre:border !prose-pre:bg-transparent">
+    <div className="prose prose-h2:mt-16 prose-img:rounded-md prose-sm prose-code:text-sm prose-pre:bg-transparent max-w-full dark:prose-invert prose-pre:border !prose-pre:bg-transparent prose-code:before:hidden prose-code:after:hidden">
       <div className="italic text-muted-foreground justify-end not-prose text-sm flex items-center gap-2">
         <p>{format(new Date(doc.date), 'yyyy年MM月dd日更新')}</p>
         <span>/</span>
